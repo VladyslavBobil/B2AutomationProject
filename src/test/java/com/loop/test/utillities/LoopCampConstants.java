3ae6317b91b96f171bc09a38cfc7d9c9d0c14767
Vladyslav <vladyslavbobil@gmail.com>
2023-11-19 15:13:21 -0500

    Day 03

---
+++ src/test/java/com/loop/test/utillities/LoopCampConstants.java
@@ -0,0 +1,6 @@
+package com.loop.test.utillities;
+
+public class LoopCampConstants {
+    public static final String EXPECTED_HEADER_FOR_FORM = "Registration form";
+    public static final String EXPECTED_PLACE_HOLDER_USERNAME = "username";
+}
